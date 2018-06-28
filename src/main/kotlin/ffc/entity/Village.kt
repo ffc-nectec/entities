e87236eeaed7c82efbb7c95a5779b09ad7393630
Piruin Panichphol <piruin.p@gmail.com>
2018-06-28 14:55:26 +0700

    Add location by centroid of place in village

--- src/main/kotlin/ffc/entity/Village.kt
+++ src/main/kotlin/ffc/entity/Village.kt
@@ -1,10 +1,14 @@
 package ffc.entity
 
+import ffc.entity.util.centroidOf
 import ffc.entity.util.generateTempId
+import me.piruin.geok.geometry.Point
 
 class Village(id: String = generateTempId()) : Entity(id) {
 
     lateinit var name: String
+    val location: Point
+        get() = centroidOf(places.filter { it.location != null }.map { it.location!! })
     var places: MutableList<Place> = mutableListOf()
     var link: Link? = null
 }
