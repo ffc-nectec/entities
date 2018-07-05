9f52409cf844e2d889d3f1b62308f08d6cc3fd2c
Thanachai Thongkum X1 Carbon2017 <singmax161@gmail.com>
2018-07-05 12:08:13 +0700

    iss person toJson not have type.

--- src/test/kotlin/ffc/entity/HouseTest.kt
+++ src/test/kotlin/ffc/entity/HouseTest.kt
@@ -23,12 +23,38 @@ class HouseTest {
 
     @Test
     fun fromJson() {
-
         val json = """
-            {"identity":{"type":"thailand-household-id","id":"10125501411"},"people":[],"link":{"isSynced":true,"lastSync":"2018-07-05T11:53:59.999+07:00","system":"JHICS","keys":{"hid":"100234"}},"no":"302/21","road":"รังสิต-นครนายก","location":{"type":"Point","coordinates":[100.5995609,14.077196]},"id":"4c9612f81718422fa11556c4f4b0c869","type":"House","timestamp":"2018-07-05T11:53:59.912+07:00"}
-        """.trimIndent()
+{
+  "identity": {
+    "type": "thailand-household-id",
+    "id": "10125501411"
+  },
+  "people": [],
+  "link": {
+    "isSynced": true,
+    "lastSync": "2018-07-05T11:53:59.999+07:00",
+    "system": "JHICS",
+    "keys": {
+      "hid": "100234"
+    }
+  },
+  "no": "302/21",
+  "road": "รังสิต-นครนายก",
+  "location": {
+    "type": "Point",
+    "coordinates": [
+      "100.5995609",
+      "14.077196"
+    ]
+  },
+  "id": "4c9612f81718422fa11556c4f4b0c869",
+  "type": "House",
+  "timestamp": "2018-07-05T11:53:59.912+07:00"
+}
+""".trimIndent()
 
         val house = json.parseTo<House>()
+
         house.no `should equal` "302/21"
     }
 }
