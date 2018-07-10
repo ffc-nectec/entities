e6ed34b9484718a1280366f81d7c7015d4cb0f34
Piruin Panichphol <piruin.p@gmail.com>
2018-07-10 11:49:18 +0700

    full json eqaul test

--- src/test/kotlin/ffc/entity/IdentityTest.kt
+++ src/test/kotlin/ffc/entity/IdentityTest.kt
@@ -1,8 +1,8 @@
 package ffc.entity
 
+import com.gregwoodfill.assert.`should equal json`
 import ffc.entity.gson.parseTo
 import ffc.entity.gson.toJson
-import org.amshove.kluent.`should contain`
 import org.amshove.kluent.`should equal`
 import org.amshove.kluent.`should not equal`
 import org.junit.Test
@@ -53,11 +53,21 @@ class IdentityTest {
 
     @Test
     fun thaiCitizenIdJsonType() {
-        ThaiCitizenId("1102304324006").toJson() `should contain` "thailand-citizen-id"
+        ThaiCitizenId("1102304324006").toJson() `should equal json` """
+{
+  "type": "thailand-citizen-id",
+  "id": "1102304324006"
+}
+"""
     }
 
     @Test
     fun thaiHouseholdIdJsonType() {
-        ThaiHouseholdId("10245003328").toJson() `should contain` "thailand-household-id"
+        ThaiHouseholdId("10245003328").toJson() `should equal json` """
+{
+  "type": "thailand-household-id",
+  "id": "10245003328"
+}
+"""
     }
 }
