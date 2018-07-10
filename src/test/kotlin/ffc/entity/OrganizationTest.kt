b36fa7a85d50a2d1a35ee066d0a685ae4b291c11
Piruin Panichphol <piruin.p@gmail.com>
2018-07-10 12:46:20 +0700

    better Test

--- src/test/kotlin/ffc/entity/OrganizationTest.kt
+++ src/test/kotlin/ffc/entity/OrganizationTest.kt
@@ -1,28 +1,37 @@
 package ffc.entity
 
+import com.gregwoodfill.assert.`should equal json`
 import ffc.entity.gson.toJson
 import org.amshove.kluent.`should be equal to`
+import org.joda.time.DateTime
+import org.junit.Ignore
 import org.junit.Test
 
 class OrganizationTest {
 
-    val org = Organization().apply {
+    val org = Organization("7e226a182d1448c69b308fb4").update<Organization>(DateTime.parse("2018-07-10T12:30:00.000+07:00")) {
         name = "รพ.สต. เนคเทค"
-        users.add(User().apply {
+        users.add(User("ad030c").update(DateTime(timestamp)) {
             name = "blast"
             password = "aaddddres"
             role = User.Role.USER
         })
-        users.add(User("123456").apply {
+        users.add(User("123456").update(timestamp) {
             name = "pcu100214.airsync"
             password = "a1e6-4f63e8295160"
             role = User.Role.ORG
         })
-        link = Link(System.JHICS, "pcucode" to 100214)
+        link = Link(System.JHICS, "pcucode" to 100214, lastSync = timestamp)
     }
 
     @Test
     fun toJson() {
+        org.toJson() `should equal json` resourceFile("Organization.json")
+    }
+
+    @Ignore
+    @Test
+    fun print() {
         println(org.toJson())
     }
 
