46c6471ecaa717c1309b174b93ca8a6d6b722cab
Piruin Panichphol <piruin.p@gmail.com>
2018-08-22 14:53:26 +0700

    redundant

--- src/main/kotlin/ffc/entity/healthcare/Disease.kt
+++ src/main/kotlin/ffc/entity/healthcare/Disease.kt
@@ -3,8 +3,7 @@ package ffc.entity.healthcare
 import ffc.entity.Entity
 import ffc.entity.util.generateTempId
 
-class Disease(val name: String, icd10: String? = null, id: String = generateTempId()) : Entity(id) {
-    var icd10: String? = icd10
+class Disease(val name: String, var icd10: String? = null, id: String = generateTempId()) : Entity(id) {
     var isChronic: Boolean = false
     var isNonCommunicable: Boolean = false
 }
