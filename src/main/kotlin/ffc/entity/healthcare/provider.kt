dc3405ae36116b53d8c44b2473e1fba33deba865
Piruin Panichphol <piruin.p@gmail.com>
2018-08-24 11:02:05 +0700

    Require providerId and patientId on create homevisit

---
+++ src/main/kotlin/ffc/entity/healthcare/provider.kt
@@ -0,0 +1,9 @@
+package ffc.entity.healthcare
+
+import ffc.entity.User
+
+fun User.homeVisit(patientId: String, serviceType: CommunityServiceType): HomeVisit {
+    require(roles.contains(User.Role.PROVIDER)) { "Only user with Provider role can use useVisit" }
+
+    return HomeVisit(id, patientId, serviceType)
+}
