c9e6bb8ceb0db59d2aa3f42d49dfc49a0ccc4dc4
Piruin Panichphol <piruin.p@gmail.com>
2018-09-07 11:36:16 +0700

    add continued

--- src/main/kotlin/ffc/entity/healthcare/Diagnosis.kt
+++ src/main/kotlin/ffc/entity/healthcare/Diagnosis.kt
@@ -1,6 +1,6 @@
 package ffc.entity.healthcare
 
-data class Diagnosis(val disease: Disease, val dxType: Type) {
+data class Diagnosis(val disease: Disease, val dxType: Type, val isContinued: Boolean = false) {
 
     enum class Type {
         PRINCIPLE_DX,
