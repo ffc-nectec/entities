48c86017f3bf0f7886d893b12409b56ef93f1c3c
Thanachai Thongkum X1 Carbon2017 <singmax161@gmail.com>
2018-10-16 15:23:57 +0700

    fixed Religion

--- src/main/kotlin/ffc/entity/place/Religion.kt
+++ src/main/kotlin/ffc/entity/place/Religion.kt
@@ -1,5 +1,29 @@
 package ffc.entity.place
 
 enum class Religion {
-    Buddhism, Christianity, Islam, Hinduism, Etc
+    Buddhism,
+    Islam,
+    Christianity,
+    Hinduism,
+    Sikhism,
+    Judaism,
+    Zen,
+    Zoroastrianism,
+    BahiFaith,
+    Etc
+}
+
+fun Religion.valueOfTh(th: String): Religion {
+    return when (th) {
+        "พุทธ" -> Religion.Buddhism
+        "อิสลาม" -> Religion.Islam
+        "คริสต์" -> Religion.Christianity
+        "ฮินดู" -> Religion.Hinduism
+        "ซิกซ์" -> Religion.Sikhism
+        "ยิว" -> Religion.Judaism
+        "เซน" -> Religion.Zen
+        "โซโรอัสเตอร์" -> Religion.Zoroastrianism
+        "บาไฮ" -> Religion.BahiFaith
+        else -> Religion.Etc
+    }
 }
