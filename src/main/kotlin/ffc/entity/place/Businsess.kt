e26c6d5b9782b47177a3291232513d38a3d2038a
Thanachai Thongkum X1 Carbon2017 <singmax161@gmail.com>
2018-10-16 11:45:25 +0700

    add Business

---
+++ src/main/kotlin/ffc/entity/place/Businsess.kt
@@ -0,0 +1,9 @@
+package ffc.entity.place
+
+import ffc.entity.Place
+import ffc.entity.util.generateTempId
+
+class Businsess(id: String = generateTempId()) : Place(id) {
+    lateinit var name: String
+    var businessType: String? = null
+}
