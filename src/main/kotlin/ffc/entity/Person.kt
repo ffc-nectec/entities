63045bcac5c3df5c9f0a0f8333f8115bbd17ce78
Thanachai Thongkum X1 Carbon2017 <singmax161@gmail.com>
2018-10-29 14:44:53 +0700

    add validate relation

--- src/main/kotlin/ffc/entity/Person.kt
+++ src/main/kotlin/ffc/entity/Person.kt
@@ -59,7 +59,11 @@ class Person(
 
     fun addRelationship(vararg pairs: Pair<Relate, Person>) {
         pairs.forEach {
-            relationships.add(Relationship(it.first, it.second))
+            val newRelation = Relationship(it.first, it.second)
+            validateRelation(id, relationships.clone().apply {
+                add(newRelation)
+            })
+            relationships.add(newRelation)
         }
     }
 
@@ -67,12 +71,79 @@ class Person(
         MALE, FEMALE, UNKNOWN
     }
 
-    class Relationship(val relate: Relate, val id: String, val name: String) {
+    class Relationship(val relate: Relate, val id: String, val name: String) : Cloneable {
 
         constructor(type: Relate, with: Person) : this(type, with.id, with.name)
+
+        fun copy(): Relationship = clone() as Relationship
     }
 
     enum class Relate {
         Father, Mother, Sibling, Twin, Married, Seperated, LegallySeperated, Divorced, Engaged, LoveAffair, Child
     }
 }
+
+private fun MutableList<Person.Relationship>.clone(): MutableList<Person.Relationship> {
+    return map {
+        it.copy()
+    }.toMutableList()
+}
+
+private val validateRelation: (String, List<Person.Relationship>) -> Unit =
+    { ownPersonId: String, updateRelation: List<Person.Relationship> ->
+
+        require(updateRelation.find { it.id == ownPersonId } == null) { "ไม่สามารถมีความสัมพันธ์กับตัวเองได้" }
+        val groupPerson: HashMap<String, ArrayList<Person.Relationship>> = hashMapOf()
+        updateRelation.forEach {
+            if (groupPerson[it.id] == null) groupPerson[it.id] = arrayListOf()
+            groupPerson[it.id]!!.add(it)
+        }
+
+        groupPerson.forEach { personId, relation ->
+            if (relation.count() > 1) {
+                val groupRelation: HashMap<Person.Relate, ArrayList<Person.Relationship>> = hashMapOf()
+                relation.forEach {
+                    if (groupRelation[it.relate] == null) groupRelation[it.relate] = arrayListOf()
+                    groupRelation[it.relate]!!.add(it)
+                }
+
+                groupRelation.forEach { relateGroup, personRelation ->
+                    require(personRelation.count() == 1) {
+                        "พบการใส่ความสัมพันธ์ซ้ำ $relateGroup count ${personRelation.count()}"
+                    }
+                    conditionRelationShip(updateRelation, personRelation, relateGroup)
+                }
+            }
+        }
+    }
+
+private fun conditionRelationShip(
+    updateRelation: List<Person.Relationship>,
+    personRelation: java.util.ArrayList<Person.Relationship>,
+    relateGroup: Person.Relate
+) {
+    val personGroupRelation = updateRelation.filter { it.id == personRelation.first().id }
+    /* ktlint-disable */
+    when (relateGroup) {
+        Person.Relate.Child -> {
+            require(personGroupRelation `ต้องไม่มีความสัมพันธ์เป็น` Person.Relate.Mother) { "ตรวจพบความสัมพันธ์ในครอบครัวแปลก $relateGroup Mother is Child" }
+            require(personGroupRelation `ต้องไม่มีความสัมพันธ์เป็น` Person.Relate.Father) { "ตรวจพบความสัมพันธ์ในครอบครัวแปลก $relateGroup Father is Child" }
+            require(personGroupRelation `ต้องไม่มีความสัมพันธ์เป็น` Person.Relate.Married) { "ตรวจพบความสัมพันธ์ในครอบครัวแปลก $relateGroup Married with child" }
+            require(personGroupRelation `ต้องไม่มีความสัมพันธ์เป็น` Person.Relate.Seperated) { "ตรวจพบความสัมพันธ์ในครอบครัวแปลก $relateGroup Separated with child" }
+            require(personGroupRelation `ต้องไม่มีความสัมพันธ์เป็น` Person.Relate.LegallySeperated) { "ตรวจพบความสัมพันธ์ในครอบครัวแปลก $relateGroup Legally Separated with child" }
+            require(personGroupRelation `ต้องไม่มีความสัมพันธ์เป็น` Person.Relate.Divorced) { "ตรวจพบความสัมพันธ์ในครอบครัวแปลก $relateGroup Divorced with child" }
+        }
+        Person.Relate.Married -> {
+            require(personGroupRelation `ต้องไม่มีความสัมพันธ์เป็น` Person.Relate.Mother) { "ตรวจพบความสัมพันธ์ในครอบครัวแปลก $relateGroup Married with mother" }
+            require(personGroupRelation `ต้องไม่มีความสัมพันธ์เป็น` Person.Relate.Father) { "ตรวจพบความสัมพันธ์ในครอบครัวแปลก $relateGroup Married with father" }
+
+            require(personGroupRelation `ต้องไม่มีความสัมพันธ์เป็น` Person.Relate.Seperated) { "ตรวจพบความสัมพันธ์ในครอบครัวแปลก $relateGroup You have to choose Married, Divorced, Separated, LegallySeperated" }
+            require(personGroupRelation `ต้องไม่มีความสัมพันธ์เป็น` Person.Relate.LegallySeperated) { "ตรวจพบความสัมพันธ์ในครอบครัวแปลก $relateGroup You have to choose Married, Divorced, Separated, LegallySeperated" }
+            require(personGroupRelation `ต้องไม่มีความสัมพันธ์เป็น` Person.Relate.Divorced) { "ตรวจพบความสัมพันธ์ในครอบครัวแปลก $relateGroup You have to choose Married, Divorced, Separated, LegallySeperated" }
+        }
+    }
+    /* ktlint-enable */
+}
+
+private infix fun List<Person.Relationship>.`ต้องไม่มีความสัมพันธ์เป็น`(relation: Person.Relate): Boolean =
+    find { it.relate == relation } == null
--- src/test/kotlin/ffc/entity/PersonRelationshipTest.kt
+++ src/test/kotlin/ffc/entity/PersonRelationshipTest.kt
@@ -2,6 +2,7 @@ package ffc.entity
 
 import org.amshove.kluent.`should contain all`
 import org.amshove.kluent.`should equal`
+import org.amshove.kluent.`should start with`
 import org.junit.Test
 
 class PersonRelationshipTest {
@@ -15,17 +16,22 @@ class PersonRelationshipTest {
     private val เมธ = male("เมธ")
     private val `ภัสสร` = female("ภัสสร")
     private val `กรกันต์` = male("กรกันต์")
+
+    private val `สมหมาย` = male("สมหมาย")
+    private val `ลูกหมี` = female("ลูกหมี")
+    private val `สมหญิง` = female("สมหญิง")
+
     private val `ประเสริฐ` = male("ประเสริฐ").apply {
         addRelationship(
-                Person.Relate.Father to อากง,
-                Person.Relate.Married to `นิภา`,
-                Person.Relate.Divorced to `คริส`,
-                Person.Relate.Mother to `อาม่า`,
-                Person.Relate.Child to `พีท`,
-                Person.Relate.Child to `ฉี`,
-                Person.Relate.Sibling to เมธ,
-                Person.Relate.Sibling to `ภัสสร`,
-                Person.Relate.Sibling to `กรกันต์`
+            Person.Relate.Father to อากง,
+            Person.Relate.Married to `นิภา`,
+            Person.Relate.Divorced to `คริส`,
+            Person.Relate.Mother to `อาม่า`,
+            Person.Relate.Child to `พีท`,
+            Person.Relate.Child to `ฉี`,
+            Person.Relate.Sibling to เมธ,
+            Person.Relate.Sibling to `ภัสสร`,
+            Person.Relate.Sibling to `กรกันต์`
         )
     }
 
@@ -68,5 +74,65 @@ class PersonRelationshipTest {
     @Test
     fun getSibling() {
         `ประเสริฐ`.siblingId `should contain all` listOf(เมธ.id, `ภัสสร`.id, `กรกันต์`.id)
+    }
+
+    @Test(expected = java.lang.IllegalArgumentException::class)
+    fun validateDuplicateRelation() {
+        try {
+            `ประเสริฐ`.apply {
+                addRelationship(Person.Relate.Married to `นิภา`)
+            }
+        } catch (ex: Exception) {
+            ex.message!! `should start with` "พบการใส่ความสัมพันธ์ซ้ำ"
+            throw ex
+        }
+    }
+
+    @Test(expected = java.lang.IllegalArgumentException::class)
+    fun validateRelationMe() {
+        try {
+            `ประเสริฐ`.apply {
+                addRelationship(Person.Relate.Married to `ประเสริฐ`)
+            }
+        } catch (ex: Exception) {
+            ex.message!! `should start with` "ไม่สามารถมีความสัมพันธ์กับตัวเองได้"
+            throw ex
+        }
+    }
+
+    @Test(expected = java.lang.IllegalArgumentException::class)
+    fun validateMotherChild() {
+        try {
+            `ประเสริฐ`.apply {
+                addRelationship(Person.Relate.Child to `อาม่า`)
+            }
+        } catch (ex: Exception) {
+            ex.message!! `should start with` "ตรวจพบความสัมพันธ์ในครอบครัวแปลก"
+            throw ex
+        }
+    }
+
+    @Test(expected = java.lang.IllegalArgumentException::class)
+    fun validateFatherChild() {
+        try {
+            `ประเสริฐ`.apply {
+                addRelationship(Person.Relate.Child to `อากง`)
+            }
+        } catch (ex: Exception) {
+            ex.message!! `should start with` "ตรวจพบความสัมพันธ์ในครอบครัวแปลก"
+            throw ex
+        }
+    }
+
+    @Test(expected = java.lang.IllegalArgumentException::class)
+    fun childMarriedFather() {
+        try {
+            `ประเสริฐ`.apply {
+                addRelationship(Person.Relate.Married to `พีท`)
+            }
+        } catch (ex: Exception) {
+            ex.message!! `should start with` "ตรวจพบความสัมพันธ์ในครอบครัวแปลก"
+            throw ex
+        }
     }
 }
