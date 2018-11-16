859e9797c61aca15a548d5cfd14640572cdf236b
Piruin Panichphol <piruin.p@gmail.com>
2018-11-16 10:47:47 +0700

    Add NCDScreen's bodymess and bp

--- src/main/kotlin/ffc/entity/healthcare/NCDScreen.kt
+++ src/main/kotlin/ffc/entity/healthcare/NCDScreen.kt
@@ -10,5 +10,10 @@ class NCDScreen(
     val smoke: Frequency = Frequency.UNKNOWN,
     val alcohol: Frequency = Frequency.UNKNOWN,
     val bloodSugar: Double? = null,
+    var weight: Double? = null,
+    var height: Double? = null,
+    var waist: Double? = null,
+    var bloodPressure: BloodPressure? = null,
+    var bloodPressure2nd: BloodPressure? = null,
     id: String = generateTempId()
 ) : Services(providerId, patientId, id)
