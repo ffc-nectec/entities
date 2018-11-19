7be1ccbfe293840aced97d6178bf5127f0f688d3
Piruin Panichphol <piruin.p@gmail.com>
2018-11-19 15:59:05 +0700

    Add Depressive analyzer

---
+++ src/main/kotlin/ffc/entity/healthcare/analyze/DepressiveAnalyzer.kt
@@ -0,0 +1,22 @@
+package ffc.entity.healthcare.analyze
+
+import ffc.entity.healthcare.Services
+import ffc.entity.healthcare.SpecialPP
+
+class DepressiveAnalyzer : Analyzer {
+
+    override val forIssue: HealthIssue.Issue = HealthIssue.Issue.DEPRESSIVE
+
+    override fun analyzeFrom(service: Services): HealthIssue? {
+        return if (service is SpecialPP) {
+            val severity = when (service.ppType.id) {
+                "1B0285" -> HealthIssue.Severity.VERY_HI
+                "1B0284" -> HealthIssue.Severity.MID
+                "1B0283" -> HealthIssue.Severity.LOW
+                "1B0281" -> HealthIssue.Severity.UNDEFINE
+                else -> null
+            }
+            if (severity != null) HealthIssue(forIssue, severity) else null
+        } else null
+    }
+}
