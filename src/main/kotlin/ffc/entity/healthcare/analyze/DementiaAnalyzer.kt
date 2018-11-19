e6ca5f27e79577f2a351ffd7c03dc03f7f231929
Piruin Panichphol <piruin.p@gmail.com>
2018-11-19 16:17:18 +0700

    update Dimentia

--- src/main/kotlin/ffc/entity/healthcare/analyze/DementiaAnalyzer.kt
+++ src/main/kotlin/ffc/entity/healthcare/analyze/DementiaAnalyzer.kt
@@ -10,7 +10,8 @@ class DementiaAnalyzer : Analyzer {
         return if (service is SpecialPP) {
             when (service.ppType.id) {
                 //"1B1220" -> HealthIssue(forIssue)
-                "1B1221", "1B1223" -> HealthIssue(forIssue)
+                "1B1221", "1B1223" -> HealthIssue(forIssue) // AMT
+                "1B1226", "1B1225" -> HealthIssue(forIssue) // MMSE-T 2002
                 else -> null
             }
         } else null
