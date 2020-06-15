bb9c61802a6417f3d4d030ef47bf84666c8ddf5b
Thanachai Thongkum X1 Carbon2017 <singmax161@gmail.com>
2020-06-15 15:25:40 +0700

    update test

--- src/test/kotlin/ffc/entity/PersonTest.kt
+++ src/test/kotlin/ffc/entity/PersonTest.kt
@@ -23,6 +23,7 @@ import ffc.entity.gson.toJson
 import ffc.entity.healthcare.Disability
 import ffc.entity.healthcare.Icd10
 import ffc.entity.util.generateTempId
+import org.amshove.kluent.`should be equal to`
 import org.amshove.kluent.`should be`
 import org.amshove.kluent.`should contain`
 import org.amshove.kluent.`should equal`
@@ -37,13 +38,42 @@ class PersonTest {
 
     val person = Person("e079e175c75a44f180e8eaeb").update(DateTime.parse("2018-06-25T14:09:07.815+07:00")) {
         identities.add(ThaiCitizenId("1154801544875"))
+        orgId = "32409198236497321949"
         prename = "นาย"
         firstname = "พิรุณ"
         lastname = "พานิชผล"
         birthDate = LocalDate.parse("1993-06-29")
         avatarUrl = "https://avatars3.githubusercontent.com/u/783403?s=460&v=4"
-        link = Link(System.JHICS, "pid" to "1234567", "cid" to "11014578451234",
-                lastSync = DateTime.parse("2018-06-25T14:09:07.815+07:00"))
+        link = Link(
+            System.JHICS, "pid" to "1234567", "cid" to "11014578451234",
+            lastSync = DateTime.parse("2018-06-25T14:09:07.815+07:00")
+        )
+    }
+
+    val person2 = Person("e079e175c75a44f180e98789").update(DateTime.parse("2018-06-25T14:09:07.815+07:00")) {
+        identities.add(ThaiCitizenId("1122233212343"))
+        orgId = "32409198236497321949"
+        prename = "นาย"
+        firstname = "สมหมาย"
+        lastname = "พารวย"
+        birthDate = LocalDate.parse("1874-12-12")
+        avatarUrl = "https://avatars3.githubusercontent.com/u/783403?s=460&v=4"
+        link = Link(
+            System.JHICS, "pid" to "1234567", "cid" to "1122233212343",
+            lastSync = DateTime.parse("2018-06-25T14:09:07.815+07:00")
+        )
+    }
+
+    @Test
+    fun relation() {
+        person.addRelationship(Person.Relate.Child to person2)
+        person2.addRelationship(Person.Relate.Father to person)
+
+        person.childId.size `should equal` 1
+        person.childId.first() `should be equal to` "e079e175c75a44f180e98789"
+        person2.fatherId!! `should be equal to` "e079e175c75a44f180e8eaeb"
+
+        person.motherId `should equal` null
     }
 
     @Test
@@ -118,7 +148,7 @@ class PersonTest {
         with(copyPerson) {
             id `should be` "piruin"
             name `should equal` "นายพิรุณ พานิชผล"
-            age `should be` 25
+            age `should be` 26
         }
     }
 
@@ -147,8 +177,9 @@ class PersonTest {
     @Test
     fun disability() {
         val disable = Disability(
-                Disability.Group.MENTAL,
-                cause = Disability.Cause.INJURED)
+            Disability.Group.MENTAL,
+            cause = Disability.Cause.INJURED
+        )
 
         person.disabilities.add(disable)
     }
